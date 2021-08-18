52ee21d87c88527d16aee1164ddcb5e63739c777
Josef Lintz <yosi1.lints@gmail.com>
2021-08-18 17:41:03 +0300

    + Added comments

--- DITest/Program.cs
+++ DITest/Program.cs
@@ -4,24 +4,50 @@
     using System.Collections.Generic;
     using System.Diagnostics;
 
-    class Program
+    public class Program
     {
+
+        /// <summary>
+        /// An enum that specifies Service lifetime
+        /// </summary>
         public enum ScopeType
         {
+            /// <summary>
+            /// Service will live as long as the process is running
+            /// </summary>
             Singleton = 0,
+
+            /// <summary>
+            /// Service will instantiated everytime it is requested
+            /// </summary>
             Transient = 1,
         };
 
 
+        /// <summary>
+        /// A simple accessor for the DI
+        /// </summary>
         private static class DI
         {
+            /// <summary>
+            /// A DI container which this class is "bound" to 
+            /// </summary>
             private static DIContainer _container;
 
+            /// <summary>
+            /// Initialize DI 
+            /// </summary>
+            /// <param name="container"> The container to initialize from </param>
             public static void Initialize(DIContainer container)
             {
                 _container = container;
             }
 
+            /// <summary>
+            /// Retrieve a service from the container
+            /// </summary>
+            /// <typeparam name="T"> The type of service </typeparam>
+            /// <returns></returns>
             public static T GetService<T>()
             {
                 return _container.GetService<T>();
@@ -29,23 +55,55 @@
         };
 
 
-        private class DIContainer
+        /// <summary>
+        /// A service container
+        /// </summary>
+        public class DIContainer
         {
-            public class ServiceItem
+
+            /// <summary>
+            /// A simple descriptor for a service
+            /// </summary>
+            public class ServiceDescriptor
             {
+                /// <summary>
+                /// The lifetime of this servie
+                /// </summary>
                 public ScopeType ScopeType { get; set; }
+
+                /// <summary>
+                /// The main Type of this service, this controls how this container finds the requested service
+                /// </summary>
                 public Type Type { get; set; }
+
+                /// <summary>
+                /// Describes the implementation if <see cref="Type"/> is an interface or other such type
+                /// </summary>
                 public Type ImplementationType { get; set; }
+
+                /// <summary>
+                /// The factory that will instantiate this service
+                /// </summary>
                 public Func<object> ImplementationFactory { get; set; }
+            };
 
-            }
 
+            /// <summary>
+            /// The main list of bound services
+            /// </summary>
+            private Dictionary<Guid, ServiceDescriptor> _services = new Dictionary<Guid, ServiceDescriptor>();
 
-            private Dictionary<Guid, ServiceItem> _services = new Dictionary<Guid, ServiceItem>();
-
+            /// <summary>
+            /// A list of singleton service that were instantiated
+            /// </summary>
             private Dictionary<Guid, object> _singletonServices = new Dictionary<Guid, object>();
 
 
+            /// <summary>
+            /// Retrieve a bound service from the container
+            /// </summary>
+            /// <typeparam name="T"> The type of service </typeparam>
+            /// <returns></returns>
             public T GetService<T>()
             {
                 Guid typeID = GetTypeID<T>();
@@ -89,59 +147,99 @@
 
 
 
+            /// <summary>
+            /// Add a Service as a singleton. 
+            /// This method requires the Service to have a parameterless constructor
+            /// </summary>
+            /// <typeparam name="T"> The service type to add</typeparam>
             public void AddSingleton<T>()
                 where T : class, new()
             {
                 Guid typeID = GetTypeID<T>();
 
-                _services.Add(typeID, new ServiceItem()
+                // Create the service descriptor
+                _services.Add(typeID, new ServiceDescriptor()
                 {
                     Type = typeof(T),
                     ImplementationType = typeof(T),
                     ScopeType = ScopeType.Singleton,
+
+                    // Since this method requires the service to have a parameterless constructor,
+                    // we can create a factory that returns an instance of T by calling said constructor
+                    ImplementationFactory = () => { return new T(); },
                 });
 
+
+                // Add the service to the list of singletons that will be later invoked when requested
                 _singletonServices.Add(typeID, null);
             }
 
+
+            /// <summary>
+            /// Add a service as a singleton, using a factory function to control how the service is created
+            /// </summary>
+            /// <typeparam name="T"> The service type </typeparam>
+            /// <param name="implementationFactory"> The factory function </param>
             public void AddSingleton<T>(Func<T> implementationFactory)
                 where T : class
             {
                 Guid typeID = GetTypeID<T>();
 
-                _services.Add(typeID, new ServiceItem()
+                // Create service descriptor
+                _services.Add(typeID, new ServiceDescriptor()
                 {
                     Type = typeof(T),
                     ScopeType = ScopeType.Singleton,
                     ImplementationFactory = () => implementationFactory(),
                 });
 
+                // Add the service to the list of singletons that will be later invoked when requested
                 _singletonServices.Add(typeID, null);
             }
 
 
+            /// <summary>
+            /// Add a service as a singleton, with an implementation.
+            /// This function requires that <typeparamref name="TImplementation"/> inherit from <typeparamref name="T"/>,
+            /// and that <typeparamref name="TImplementation"/> has a parameterless constructor
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            /// <typeparam name="TImplementation"></typeparam>
             public void AddSingelton<T, TImplementation>()
                 where T : class
+                where TImplementation : T, new()
             {
                 Guid typeID = GetTypeID<T>();
 
-                _services.Add(typeID, new ServiceItem()
+                // Create service descriptor
+                _services.Add(typeID, new ServiceDescriptor()
                 {
                     Type = typeof(T),
                     ImplementationType = typeof(TImplementation),
                     ScopeType = ScopeType.Singleton,
+                    ImplementationFactory = () => { return new TImplementation(); },
                 });
 
+                // Add the service to the list of singletons that will be later invoked when requested
                 _singletonServices.Add(typeID, null);
             }
 
+
+            /// <summary>
+            /// Add a service as a singleton, with an implementation.
+            /// This function requires that <typeparamref name="TImplementation"/> inherit from <typeparamref name="T"/>
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            /// <typeparam name="TImplementation"></typeparam>
+            /// <param name="implementationFactory"></param>
             public void AddSingleton<T, TImplementation>(Func<TImplementation> implementationFactory)
                 where T : class
                 where TImplementation : T
             {
                 Guid typeID = GetTypeID<T>();
 
-                _services.Add(typeID, new ServiceItem()
+                // Create service descriptor
+                _services.Add(typeID, new ServiceDescriptor()
                 {
                     Type = typeof(T),
                     ImplementationType = typeof(TImplementation),
@@ -149,17 +247,25 @@
                     ImplementationFactory = () => implementationFactory(),
                 });
 
+                // Add the service to the list of singletons that will be later invoked when requested
                 _singletonServices.Add(typeID, null);
             }
 
 
 
+
+            /// <summary>
+            /// Add a service as a transient.
+            /// This method requires <typeparamref name="T"/> to have a parameterless constructor
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
             public void AddTransient<T>()
                 where T : class, new()
             {
                 Guid typeID = GetTypeID<T>();
 
-                _services.Add(typeID, new ServiceItem()
+                // Create service descriptor
+                _services.Add(typeID, new ServiceDescriptor()
                 {
                     Type = typeof(T),
                     ImplementationType = typeof(T),
@@ -167,17 +273,21 @@
 
                     ImplementationFactory = () => { return new T(); },
                 });
-
-                // _transientServices.Add(typeID, null);
             }
 
-            public void AddTransient<T, TImplementation>(Func<TImplementation> implementationFactory)
+
+            /// <summary>
+            /// Add a service as a transient, with a factory function to control how the service is created
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            /// <param name="implementationFactory"></param>
+            public void AddTransient<T>(Func<T> implementationFactory)
                 where T : class
-                where TImplementation : T
             {
                 Guid typeID = GetTypeID<T>();
 
-                _services.Add(typeID, new ServiceItem()
+                // Create service descriptor
+                _services.Add(typeID, new ServiceDescriptor()
                 {
                     Type = typeof(T),
                     ImplementationType = typeof(T),
@@ -185,17 +295,73 @@
 
                     ImplementationFactory = () => implementationFactory(),
                 });
-
-                // _transientServices.Add(typeID, null);
             }
 
 
+            /// <summary>
+            /// Add a service as a transient, with an implementation.
+            /// This function requires that <typeparamref name="TImplementation"/> inherit from <typeparamref name="T"/>,
+            /// and that <typeparamref name="TImplementation"/> has a parameterless constructor
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            /// <typeparam name="TImplementation"></typeparam>
+            public void AddTransient<T, TImplementation>()
+                where T : class
+                where TImplementation : T, new()
+            {
+                Guid typeID = GetTypeID<T>();
+
+                // Create service descriptor
+                _services.Add(typeID, new ServiceDescriptor()
+                {
+                    Type = typeof(T),
+                    ImplementationType = typeof(TImplementation),
+                    ScopeType = ScopeType.Transient,
+
+                    ImplementationFactory = () => { return new TImplementation(); },
+                });
+
+            }
+
+
+            /// <summary>
+            /// Add a service as a transient, with an implementation.
+            /// This function requires that <typeparamref name="TImplementation"/> inherit from <typeparamref name="T"/>
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            /// <typeparam name="TImplementation"></typeparam>
+            /// <param name="implementationFactory"></param>
+            public void AddTransient<T, TImplementation>(Func<TImplementation> implementationFactory)
+                where T : class
+                where TImplementation : T
+            {
+                Guid typeID = GetTypeID<T>();
+
+                // Create service descriptor
+                _services.Add(typeID, new ServiceDescriptor()
+                {
+                    Type = typeof(T),
+                    ImplementationType = typeof(TImplementation),
+                    ScopeType = ScopeType.Transient,
+
+                    ImplementationFactory = () => implementationFactory(),
+                });
+            }
+
+
+            /// <summary>
+            /// Get a unique ID from a Type
+            /// </summary>
+            /// <typeparam name="T"></typeparam>
+            /// <returns></returns>
             private Guid GetTypeID<T>()
             {
+                // Thank you microsoft for making my life easier
                 return typeof(T).GUID;
             }
 
         };
+
 
 
         public interface ITestClassSingelton
@@ -263,7 +429,7 @@
 
         public interface ITestClassTransient2
         {
-            public string GUID { get;  }
+            public string GUID { get; }
         }
 
         public class TestClass2Transient : ITestClassTransient2
@@ -271,7 +437,7 @@
             private TestClassSingelton _testClass;
 
             public string GUID { get; } = Guid.NewGuid().ToString("N");
-            
+
             public TestClass2Transient(string guid)
             {
                 GUID = guid;
@@ -279,7 +445,7 @@
         }
 
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             DIContainer di = new DIContainer();
 
@@ -305,6 +471,10 @@
 
 
             di.AddTransient<TestClassTransient>();
+            di.AddTransient<TestClass2Transient>(() =>
+            {
+                return new TestClass2Transient("2");
+            });
             di.AddTransient<ITestClassTransient, TestClassTransient>(() =>
             {
                 return new TestClassTransient();
@@ -465,6 +635,23 @@
                         Debugger.Break();
                 };
 
+
+                {
+                    var instance = DI.GetService<TestClass2Transient>();
+                    var instance2 = DI.GetService<TestClass2Transient>();
+
+                    if (instance == instance2)
+                        Debugger.Break();
+                };
+
+                {
+                    var instance = DI.GetService<TestClass2Transient>();
+                    var instance2 = DI.GetService<TestClass2Transient>();
+
+                    if ((instance.GUID != "2") &&
+                        (instance2.GUID != "2"))
+                        Debugger.Break();
+                };
 
             };
 
